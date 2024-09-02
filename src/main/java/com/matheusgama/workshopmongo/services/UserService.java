c6f35d914ebcf42e39b809fd521e57e24dcd2ab0
Matheus Gama <mathemovic.mg@gmail.com>
2024-09-02 15:53:09 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/matheusgama/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.matheusgama.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusgama.workshopmongo.domain.User;
+import com.matheusgama.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+}
